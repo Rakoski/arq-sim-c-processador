9250a1199ddc598f4bdffae4b58394cc37c3d0a5
Luqui05 <lucashtribeiro@hotmail.com>
2024-10-01 16:11:44 -0300

    feat: Implementação da decodificação no pipeline.c
    Foi criada a function 'decodifica' e a 'main' foi modificada para incluir a decodificação, com algumas impressões para depuração

--- pipeline.c
+++ pipeline.c
@@ -89,6 +89,27 @@ void busca(EstadoDaPipeline *estado_da_pipeline, const uint16_t *memoria) {
     estado_da_pipeline->busca.pc++;
 }
 
+void decodifica(EstadoDaPipeline *estado) {
+  uint16_t instrucao = estado->busca.instrucao;
+
+  //determinar tipo de instrução(r ou i)
+  estado->decodifica.tipo = (instrucao >> 15) ? 'I' : 'R';
+
+  if (estado->decodifica.tipo == 'R') {
+    estado->decodifica.opcode = (instrucao >> 9) & 0x3F; // 6 bits do opcode
+    estado->decodifica.reg_dest = (instrucao >> 6) & 0x7; // 3 bits do reg_dest
+    estado->decodifica.reg1 = (instrucao >> 3) & 0x7; // 3 bits do reg1
+    estado->decodifica.reg2 = instrucao & 0x7; // 3 bits do reg2
+    estado->decodifica.imediato = 0; // não se usa imediato em instruções 'R'
+  } else {
+    estado->decodifica.opcode = (instrucao >> 13) & 0x3; // 2 bits do opcode
+    estado->decodifica.reg_dest = (instrucao >> 10) & 0x7; // 3 bits do reg_dest
+    estado->decodifica.imediato = instrucao & 0x3FF; // 10 bits do imediato
+    estado->decodifica.reg1 = 0; // não se usa reg1 em instruções 'I'
+    estado->decodifica.reg2 = 0; // o mesmo se aplica para reg 2
+  }
+}
+
 int main(const int argc, char **argv) {
     if (argc != 2) {
         printf("usage: %s [bin_name]\n", argv[0]);
@@ -101,6 +122,18 @@ int main(const int argc, char **argv) {
 
     while (1) {
         busca(&estado_da_pipeline, memoria);
+        decodifica(&estado_da_pipeline);
+
+        //impressões de depuracao
+        printf("Instrução: 0x%04X\n", estado_da_pipeline.busca.instrucao);
+        printf("Tipo: %c\n", estado_da_pipeline.decodifica.tipo);
+        printf("Opcode: %d\n", estado_da_pipeline.decodifica.opcode);
+        printf("Reg Dest: %d\n Reg1: %d\n Reg2: %d\n Imediato: %d\n",
+               estado_da_pipeline.decodifica.reg_dest,
+               estado_da_pipeline.decodifica.reg1,
+               estado_da_pipeline.decodifica.reg2,
+               estado_da_pipeline.decodifica.imediato);
+
         getchar();
     }
 
